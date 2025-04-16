71540ca63cc387cb47ab7fbb80047113ae45290d
tubolur <chugurutop987@proton.me>
2025-04-16 20:46:05 +0000

    Update runHTTPS.py

--- runHTTPS.py
+++ runHTTPS.py
@@ -1,4 +1,4 @@
 from modules.HTTPSServer import *
-UTILS=os.path.join(os.path.abspath(__file__),"utils")
+UTILS=os.path.join(os.path.split(os.path.abspath(__file__))[0],"utils")
 s=HTTPSServer(keyPath=os.path.join(UTILS,"key.pem"),certPath=os.path.join(UTILS,"cert.pem"));
 print("Hit ctrl+c to exit");
