9d05df40b8bea7646ed71801142e853c10ccf451
tubolur <chugurutop987@proton.me>
2025-04-25 18:27:03 +0000

    Add files via upload

---
+++ runMessageBoard.py
@@ -0,0 +1,18 @@
+import os
+from modules.MessageBoard import *
+from modules.HTTPSServer import *
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+WORKINGDIR=os.path.join(os.path.join(os.path.join(os.getenv("HOME"),'dev'),'servertools'));
+
+if not os.path.exists(WORKINGDIR):
+        os.mkdir(WORKINGDIR)
+
+UTILSDIR=os.path.join(WORKINGDIR,"utils")
+
+rh=MessageBoardHTTPRequestHandler;
+mb=MessageBoard(databasePath=os.path.join(UTILSDIR,"database.db"),workingPath=os.path.join(WORKINGDIR,"mb"),requestHandler=rh);#,requestHandler=rh
+
+s=HTTPSServer(keyPath=os.path.join(UTILSDIR,"key.pem"),certPath=os.path.join(UTILSDIR,"cert.pem"),requestHandler=rh);#,requestHandler=rh
+print("Hit ctrl+c to exit");
