c057f2abdd16e98e943c1804063e4c44e1b8e788
tubolur <chugurutop987@proton.me>
2025-09-14 21:08:26 +0200

    Update runHTTPS.py

--- runHTTPS.py
+++ runHTTPS.py
@@ -1,15 +1,16 @@
 from modules.HTTPSServer import *
 import sys
 
-if len(sys.argv)>1:
-        WORKINGDIR=sys.argv[1];
-else:
-        WORKINGDIR=os.path.join(os.path.join(os.path.join(os.getenv("HOME"),'dev'),'servertools'));
+
+WORKINGDIR=os.path.join(os.path.join(os.path.join(os.getenv("HOME"),'dev'),'servertools'));
 
 if not os.path.exists(WORKINGDIR):
         os.mkdir(WORKINGDIR)
 
-UTILSDIR=os.path.join(WORKINGDIR,"utils")
+if len(sys.argv)>1:
+        UTILSDIR=sys.argv[1];
+else:
+        UTILSDIR=os.path.join(WORKINGDIR,"utils")
 
 s=HTTPSServer(keyPath=os.path.join(UTILSDIR,"key.pem"),certPath=os.path.join(UTILSDIR,"cert.pem"));
 print("Hit ctrl+c to exit");
